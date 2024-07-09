53e46536b6a7fcbb0c0e955706c3baad70032889
Wenxia Sweeney <52976642+w3nx14@users.noreply.github.com>
2024-07-08 22:12:03 -0500

    Version 2 progress report A
    Fixed code. Now I can track an expense, export to a CSV, and return how much money per day someone can spend in their budget. I will work on implementing your suggestions next time.

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,55 +1,65 @@
-from expenses import Expense
+from expense import Expense
+import calendar
+import datetime
+
 
 def main():
-    print(f"Running Expense Tracker!")
+    print(f"🎯 Running Expense Tracker!")
     expense_file_path = "expenses.csv"
+    budget = 2000
 
-    # Get user input for expense
-    #expense = get_user_expense()
+    # Get user input for expense.
+    expense = get_user_expense()
 
-    # Write their expense to a file
-    #save_expense_to_file(expense, expense_file_path)
+    # Write their expense to a file.
+    save_expense_to_file(expense, expense_file_path)
 
-    # Read file and summarize expenses
-    summarize_expenses(expense_file_path)
+    # Read file and summarize expenses.
+    summarize_expenses(expense_file_path, budget)
 
-    pass
 
 def get_user_expense():
-    print(f"Getting User Expense")
-    expense_name = input("What did you buy?")
-    expense_amount = float(input("How much did it cost?"))
-   # expense_categories = ["🍽️  Food", "💸  Bills", "💼 Work", "💳 Debt", "🎉 Fun", "✨ Misc"]
-    expense_categories = ["Food", "Bills", "Work", "Debt", "Fun", "Misc"]
+    print(f"🎯 Getting User Expense")
+    expense_name = input("Enter expense name: ")
+    expense_amount = float(input("Enter expense amount: "))
+    expense_categories = ["🍽️  Food", "💸  Bills", "💼 Work", "💳 Debt", "🎉 Fun", "✨ Misc"]
 
     while True:
         print("Select a category: ")
         for i, category_name in enumerate(expense_categories):
-            print(f"    {i + 1}. {category_name}")
+            print(f"  {i + 1}. {category_name}")
 
         value_range = f"[1 - {len(expense_categories)}]"
         selected_index = int(input(f"Enter a category number {value_range}: ")) - 1
 
-        if selected_index in range (len(expense_categories)):
+        if selected_index in range(len(expense_categories)):
             selected_category = expense_categories[selected_index]
-            new_expense = Expense(name=expense_name, category=selected_category, amount=expense_amount)
+            new_expense = Expense(
+                name=expense_name, category=selected_category, amount=expense_amount
+            )
             return new_expense
         else:
             print("Invalid category. Please try again!")
 
-def save_expense_to_file(expense, expense_file_path):
-    print(f"Saving User Expense: {expense} to {expense_file_path}")
+
+def save_expense_to_file(expense: Expense, expense_file_path):
+    print(f"🎯 Saving User Expense: {expense} to {expense_file_path}")
     with open(expense_file_path, "a") as f:
         f.write(f"{expense.name},{expense.amount},{expense.category}\n")
 
+
 def summarize_expenses(expense_file_path, budget):
-    print(f"Summarizing User Expense")
+    print(f"🎯 Summarizing User Expense")
     expenses: list[Expense] = []
     with open(expense_file_path, "r") as f:
         lines = f.readlines()
         for line in lines:
             expense_name, expense_amount, expense_category = line.strip().split(",")
-            line_expense = Expense(name=expense_name, amount=float(expense_amount), category=expense_category,)
+            line_expense = Expense(
+                name=expense_name,
+                amount=float(expense_amount),
+                category=expense_category,
+            )
             expenses.append(line_expense)
 
     amount_by_category = {}
@@ -60,7 +70,7 @@ def summarize_expenses(expense_file_path, budget):
         else:
             amount_by_category[key] = expense.amount
 
-    print("Expenses By Category:")
+    print("Expenses By Category 📈:")
     for key, amount in amount_by_category.items():
         print(f"  {key}: ${amount:.2f}")
 
@@ -75,7 +85,12 @@ def summarize_expenses(expense_file_path, budget):
     remaining_days = days_in_month - now.day
 
     daily_budget = remaining_budget / remaining_days
-    print(green(f"Budget Per Day: ${daily_budget:.2f}"))
+    print(green(f"👉 Budget Per Day: ${daily_budget:.2f}"))
+
+
+def green(text):
+    return f"\033[92m{text}\033[0m"
+
 
 if __name__ == "__main__":
     main()
